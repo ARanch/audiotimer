5fdfcf65d82eb5d41be44d5d9de67b2953e76f77
Andreas Ranch <ranch@sowasound.com>
2024-01-05 00:49:03 +0100

    Remove env directory and update .gitignore

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # read the contents of your README file
 from pathlib import Path
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='audiotimer',
-    version='0.1',
+    version='0.2.2',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown'
