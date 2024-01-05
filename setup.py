240bb69ee867230feea9140943ff7b5e41ce0421
Andreas Ranch <ranch@sowasound.com>
2024-01-05 01:17:12 +0100

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='audiotimer',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -16,4 +16,9 @@ setup(
             'audiotimer=audiotimer.__main__:main'
         ]
     },
+    install_requires=[
+        'numpy>=1.26.3',
+        'PyAudio>=0.2.14',
+        'pynput>=1.7.6'
+    ]
 )
